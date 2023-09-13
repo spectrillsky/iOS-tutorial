2a63602721713142fcfa5ad4f3d3b38cfda63f14
spectrillsky <skypniewski@gmail.com>
2023-09-13 11:23:23 -0400

    Initial push

--- iOS-tutorial/iOS_tutorialApp.swift
+++ iOS-tutorial/iOS_tutorialApp.swift
@@ -10,8 +10,8 @@ import SwiftUI
 @main
 struct iOS_tutorialApp: App {
     var body: some Scene {
-        WindowGroup {
-            ContentView()
+        WindowGroup {	
+            MeetingView()
         }
     }
 }
