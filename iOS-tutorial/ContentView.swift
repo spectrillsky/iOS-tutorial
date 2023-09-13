692f3b1344052a9da6e8e2da395bdf4e4073de92
spectrillsky <skypniewski@gmail.com>
2023-09-13 10:52:29 -0400

    Initial Commit

---
+++ iOS-tutorial/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  iOS-tutorial
+//
+//  Created by Skylar Pniewski on 9/13/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS-tutorial/iOS_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_tutorialApp.swift
+//  iOS-tutorial
+//
+//  Created by Skylar Pniewski on 9/13/23.
+//
+
+import SwiftUI
+
+@main
+struct iOS_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
